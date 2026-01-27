5bcf74a00ad77c21b8b19cb169fa25fc063773b5
darmaxaguameli-ai <darmaxmeli@gmail.com>
2026-01-27 13:47:33 -0600

    feat: Refactor Prisma client initialization to improve singleton pattern handling

--- server/lib/prisma.js
+++ server/lib/prisma.js
@@ -1,13 +1,15 @@
 // lib/prisma.js
 import { PrismaClient } from '@prisma/client';
 
-// Official Prisma Client singleton pattern
-// See: https://www.prisma.io/docs/guides/database/troubleshooting-orm/database-connections#prevent-hot-reloading-from-creating-new-instances-of-prismaclient
+let prisma;
 
-const globalForPrisma = globalThis;
-
-const prisma = globalForPrisma.prisma ?? new PrismaClient();
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
 
 export default prisma;
